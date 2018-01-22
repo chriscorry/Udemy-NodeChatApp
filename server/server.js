498e35310ae0a7e7e853f3e847c25411bc494f13
Chris Corry <chris@thecorrys.com>
2018-01-21 21:53:54 -0800

    Greet new users and alert the chat rom.

--- server/server.js
+++ server/server.js
@@ -19,18 +19,40 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    sender: 'Admin',
+    text: 'Welcome to the chat channel! Keep it clean, yo.',
+    createdAt: Date.now()});
+
+  socket.broadcast.emit('newMessage', {
+    sender: 'Admin',
+    text: 'A new user has joined the chat room.',
+    createdAt: Date.now()});
 
   socket.on('createMessage', (body) => {
     console.log('Received new message');
 
-    io.emit('newMessage', {
+    // // Sends to everyone -- note call on io and not socket
+    // io.emit('newMessage', {
+    //   sender: body.sender,
+    //   text: body.text,
+    //   createdAt: Date.now()});
+
+    // Sends to everyone except this socket
+    socket.broadcast.emit('newMessage', {
       sender: body.sender,
       text: body.text,
       createdAt: Date.now()});
+
   });
 
   socket.on('disconnect', (socket) => {
     console.log('User disconnected');
+
+    io.emit('newMessage', {
+      sender: 'Admin',
+      text: 'A user has left the chat room.',
+      createdAt: Date.now()});
   });
 });
 
